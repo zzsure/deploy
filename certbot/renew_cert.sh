2f38fc8a7258a51651e9611c93db441dedaa3594
zzs <z@azbit.cn>
2020-01-22 11:02:53 +0800

    add fin.azbit.cn certbot

--- certbot/renew_cert.sh
+++ certbot/renew_cert.sh
@@ -1,13 +1,22 @@
 # Renew Certificate
+# blog.azbit.cn
 docker run -it --rm --name certbot \
     -v "/root/deploy/certbot/ssl:/etc/letsencrypt" \
     -v "/root/deploy/wordpress/src:/var/www/html" \
     certbot/certbot certonly -n --no-eff-email --email zhzosh@gmail.com --agree-tos --webroot -w /var/www/html -d blog.azbit.cn
 
+# img.azbit.cn
 docker run -it --rm --name certbot \
     -v "/root/deploy/certbot/ssl:/etc/letsencrypt" \
     -v "/root/deploy/chevereto/src:/var/www/html" \
     certbot/certbot certonly -n --no-eff-email --email zhzosh@gmail.com --agree-tos --webroot -w /var/www/html -d img.azbit.cn
+
+# fin.azbit.cn
+docker run -it --rm --name certbot \
+    -v "/root/deploy/certbot/ssl:/etc/letsencrypt" \
+    -v "/data/certbot/log:/var/log/letsencrypt" \
+    -v "/root/deploy/nginx/www/letsencrypt:/var/www/.well-known" \
+    certbot/certbot certonly -n --no-eff-email --email zhzosh@gmail.com --agree-tos --webroot -w /var/www -d fin.azbit.cn
 
 # reload nginx
 docker restart nginx
